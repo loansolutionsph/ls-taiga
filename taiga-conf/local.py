39437ffaee6f2682419eec4115815dcd527e29cd
Raymond Torino <torino.raymond@gmail.com>
2015-12-08 03:23:05 +0800

    chore: add email settings

--- taiga-conf/local.py
+++ taiga-conf/local.py
@@ -17,3 +17,12 @@ INSTALLED_APPS += ["taiga_contrib_slack"]
 ## LDAP
 # see https://github.com/ensky/taiga-contrib-ldap-auth
 # INSTALLED_APPS += ["taiga_contrib_ldap_auth"]
+
+# EMAIL SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'email-smtp.us-west-2.amazonaws.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'AKIAICN6QFIQC5KVOVYA'
+EMAIL_HOST_PASSWORD = 'AtOqwrp5ichBpqoKsW2vF3JS4ha8gDrUD1bjY0dxXgld'
+DEFAULT_FROM_EMAIL = 'sending@loansolutions.ph'
